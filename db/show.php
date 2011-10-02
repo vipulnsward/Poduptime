15645c1c50a092bccbd52af900d9c944295f9bdf
David Morley <dia@z8s.com>
2011-10-02 14:42:40 -0500

    show ratings - non functioning yet

--- db/show.php
+++ db/show.php
@@ -1,5 +1,6 @@
+<meta charset="utf-8"> 
 <!-- /* Copyright (c) 2011, David Morley. This file is licensed under the Affero General Public License version 3 or later. See the COPYRIGHT file. */ -->
-<table id="myTable" class="tablesorter" width="75%">
+<table id="myTable" class="tablesorter" width="98%">
 <thead>
 <tr>
 <th width="220px">Diaspora Pod<a class="tipsy" title="A pod is a site for you to set up your account.">?</a></th>
@@ -8,6 +9,7 @@
 od last updated from the main Diaspora code.">?</a></th>
 <th>Uptime<a class="tipsy" title="Percent of the time the pod is online for <?php echo date("F") ?>.">?</a></th>
 <th>Months<a class="tipsy" title="How many months has this pod been online? Click number for more history.">?</a></th>
+<th>Rating<a class="tipsy" title="User and Admin rating for this pod.">?</a></th>
 <th>Response Time<a class="tipsy" title="Average response time for <?php echo date("F") ?>.">?</a></th>
 <th>Ipv6<a class="tipsy" title="Does this pod look to have ipv6">?</a></th>
 </tr>
@@ -30,11 +32,23 @@ od last updated from the main Diaspora code.">?</a></th>
  }   
  while ($row = pg_fetch_array($result)) {
 if ($row["secure"] == "true") {$method = "https://";$class="green";$tip="This pod uses SSL encryption for traffic.";} else {$method = "http://";$class="red";$tip="This pod does not offer SSL";} 
-     echo "<tr><td class='tipsy' title='".$tip."'><a class='$class' target='new' href='". $method . $row["domain"] ."'>" . $method . $row["domain"] . "</a></td>";
+     echo "<tr><td><div title='$tip' class='tipsy'><a class='$class' target='new' href='". $method . $row["domain"] ."'>" . $method . $row["domain"] . "</a></div></td>";
      echo "<td>" . $row["status"] . "</td>";
-     echo "<td class='tipsy' title='Git Revision ".$row["hgitref"]."'><div id='".$row["hgitdate"]."' class='utc-timestamp'>" . strtotime($row["hgitdate"]) . "</div></td>";
+     echo "<td><div class='tipsy' title='Git Revision ".$row["hgitref"]."'><div id='".$row["hgitdate"]."' class='utc-timestamp'>" . strtotime($row["hgitdate"]) . "</div></div></td>";
      echo "<td>" . $row["uptimelast7"] . "</td>";
-     echo "<td class='tipsy' title='Last Check ".$row["dateupdated"]." '><a target='new' href='".$row["pingdomurl"]."'>" . $row["monthsmonitored"] . "</a></td>";
+     echo "<td><div title='Last Check ".$row["dateupdated"]."' class='tipsy'><a target='new' href='".$row["pingdomurl"]."'>" . $row["monthsmonitored"] . "</a></div></td>";
+if ($row["userrating"] >6) {$userratingclass="green";} elseif ($row["userrating"] <7) {$userratingclass="yellow";} elseif ($row["userrating"] <3) {$userratingclass="red";}
+     echo "<td><div class='tipsy rating ".$userratingclass."' title='User rating is ".$row["adminrating"]."'>";
+for ($i = 0; $i < $row["userrating"]; $i++) { 
+echo "✪";
+}
+if ($row["adminrating"] >6) {$adminratingclass="green";} elseif ($row["adminrating"] <7) {$adminratingclass="yellow";} elseif ($row["adminrating"] <3) {$adminratingclass="red";}
+     echo "</div><br><div class='tipsy rating ".$adminratingclass."' title='Poduptime rating is ".$row["adminrating"]."'>";
+for ($i = 0; $i < $row["adminrating"]; $i++) {
+echo "✪";
+}
+
+     echo "</div></td>";
      echo "<td>" . $row["responsetimelast7"] . "</td>";
      echo "<td class='tipsy' title='IP Address ".$row["ip"]." '>" . $row["ipv6"] . "</td></tr>\n";
  }
