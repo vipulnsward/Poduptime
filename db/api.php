3a1de406d0c411572d33c7d85fb6d20a53189543
Vipul A M <vipulnsward@gmail.com>
2011-12-02 00:25:02 +0530

    Fixed JSON Object Response

--- db/api.php
+++ db/api.php
@@ -40,62 +40,19 @@ echo "</feed>";
 }
 
 elseif ($_GET['format'] == "json") {
- $i=0;
+ //Create a JSON response
  $sql = "SELECT * FROM pods";
  $result = pg_query($dbh, $sql);
  if (!$result) {
      die("Error in SQL query: " . pg_last_error());
  }
  $numrows = pg_num_rows($result);
-//json output WIP
-header('Content-type: application/json');
-echo '{';
-echo '"podcount": '.json_encode($numrows);
-//
-echo '"pods": [';
- while ($row = pg_fetch_array($result)) {
+ header('Content-type: application/json');
+ $rows=array_values(pg_fetch_all($result));
+ $obj->podcount          = $numrows;
+ $obj->pods             = $rows;
+ print json_encode($obj);
 
-  echo '{';
-  echo '"url":"'.$method.$row["domain"].'",';
-  echo '"email":"private",';
-  echo '"whois":"'.$method.$row["whois"].'",';
-  echo '"city":"'.$method.$row["city"].'",';
-  echo '"state":"'.$method.$row["state"].'",';
-  echo '"country":"'.$method.$row["country"].'",';
-  echo '"latitude":"'.$method.$row["lat"].'",';
-  echo '"longitude":"'.$method.$row["long"].'",';
-  echo '"postalcode":"'.$method.$row["postalcode"].'",';
-  echo '"connection":"'.$method.$row["connection"].'",';
-  echo '"SSL Valid":"'.$method.$row["sslvalid"].'",';
-  echo '"secure":"'.$method.$row["secure"].'",';
-  echo '"status":"'.$method.$row["status"].'",';
-  echo '"ip":"'.$method.$row["ip"].'",';
-  echo '"ipv6":"'.$method.$row["ipv6"].'",';
-  echo '"Git Last":"'.$method.$row["hgitdate"].'",';
-  echo '"Git Rev":"'.$method.$row["hgitref"].'",';
-  echo '"Pingdom Url":"'.$method.$row["pingdomurl"].'",';
-  echo '"Pingdom Last":"'.$method.$row["pingdomlast"].'",';
-  echo '"Months Monitored":'.$method.$row["monthsmonitored"].',';
-  echo '"Uptime":"'.$method.$row["uptimelast7"].'",';
-  echo '"Responsetime":"'.$method.$row["responsetimelast7"].'",';
-  echo '"Server Runtime":"'.$method.$row["hruntime"].'",';
-  echo '"Server Encoding":"'.$method.$row["hencoding"].'",';
-  echo '"Date Added":"'.$method.$row["dateCreated"].'",';
-  echo '"Date Updated":"'.$method.$row["dateUpdated"].'",';
-  echo '"Date Stats Updated":"'.$method.$row["dateLaststats"].'",';
-  echo '"hidden":"'.$method.$row["hidden"].'"';
-
-//
-//
-  echo '}';
-if ($i < ($numrows -1)) {
-  echo ',';
-}
-$i++;
-}
-echo ']';
-
-echo '}';
 } else {
  $i=0;
  $sql = "SELECT * FROM pods WHERE hidden <> 'yes' ORDER BY Hgitdate DESC, uptimelast7 DESC";
